76f9cc68dbb26496ed07b8f8addc889d9f1c3a5a
ashikka <ashikagupta28@gmail.com>
2023-05-10 18:55:16 +0530

    feat: 404 page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -18,6 +18,7 @@ import LoadingScreen from './pages/LoadingScreen/LoadingScreen';
 import { UserContextProvider } from './contexts/user';
 import ProfileScreen from './pages/ProfileScreen/ProfileScreen';
 import ReviewPaper from './pages/ReviewPaper/ReviewPaper';
+import PageNotFoundScreen from './pages/PageNotFoundScreen/PageNotFoundScreen';
 
 
 function App() {
@@ -34,6 +35,7 @@ function App() {
               <Route path="/" element={<SignUpLoginInScreen />} />
               <Route path="/profile" element={<ProfileScreen />} />
               <Route path="/review" element={<ReviewPaper />} />
+              <Route path="*" element={<PageNotFoundScreen />} />
             </>
           </Routes>
         </UserContextProvider>
