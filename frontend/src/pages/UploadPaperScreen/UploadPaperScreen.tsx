65e1e8b88825ca2a9a2c10410124c92f96b0fd58
ashikka <ashikagupta28@gmail.com>
2023-05-10 16:50:00 +0530

    feat: Added abstract field

--- frontend/src/pages/UploadPaperScreen/UploadPaperScreen.tsx
+++ frontend/src/pages/UploadPaperScreen/UploadPaperScreen.tsx
@@ -2,7 +2,7 @@ import { useContext, useEffect, useRef, useState } from 'react';
 import { ApiContext } from '../../contexts/api';
 import { EtherContext } from '../../contexts/ether';
 import { ChevronLeftIcon, ChevronRightIcon, LinkIcon } from '@chakra-ui/icons'
-import { Flex, Heading, Button, OrderedList, ListItem, FormControl, FormLabel, Input, Progress, Badge } from '@chakra-ui/react';
+import { Flex, Heading, Button, OrderedList, ListItem, FormControl, FormLabel, Input, Progress, Badge, Select, Textarea } from '@chakra-ui/react';
 import logo from '../../assets/logo.png';
 import LoadingScreen from '../LoadingScreen/LoadingScreen';
 import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
@@ -11,12 +11,16 @@ import withReactContent from 'sweetalert2-react-content'
 
 
 export default function UploadPaperScreen() {
+    const [title, setTitle] = useState("");
+    const [category, setCategory] = useState("");
+    const [abstract, setAbstract] = useState("");
     const [url, setUrl] = useState('');
     const [numPages, setNumPages] = useState(0);
     const [pageNumber, setPageNumber] = useState(1);
     const file = useRef(null);
     const [progress, setProgress] = useState(0);
     const [reviewFileScreen, setReviewFileScreen] = useState(false);
+    const [abstractScreen, setAbstractScreen] = useState(false);
     const ether = useContext(EtherContext).ether;
     const api = useContext(ApiContext).api;
     const [loading, setLoading] = useState(true);
@@ -36,6 +40,14 @@ export default function UploadPaperScreen() {
         )
     }
 
+    const showOptions = () => {
+        return (
+            <Select placeholder='Select from dropdown' value={category} onChange={e => setCategory(e.target.value)}>
+                {["Physical, Chemical and Earth Sciences", "Humanities and Creative Arts", "Social Sciences", "Business and Management", "Engineering and Technology", "Education", "Law", "Health and Medicine", "Agriculture and Environment", "Other"].map((category) => <option>{category}</option>)}
+            </Select>
+        )
+    }
+
     const retrieveFile = async (e: any) => {
         if (ether == null || !e.target.files) return;
 
@@ -45,9 +57,12 @@ export default function UploadPaperScreen() {
     }
 
     const reviewFile = async () => {
-        if (file.current) {
+        if (file.current && title && category) {
             console.log(file.current)
-            setReviewFileScreen(true);
+            setAbstractScreen(true);
+            if (abstract) {
+                setReviewFileScreen(true);
+            }
         } else {
             MySwal.fire({
                 icon: 'error',
@@ -69,7 +84,7 @@ export default function UploadPaperScreen() {
         MySwal.fire({
             icon: 'success',
             title: <p>Good Job!</p>,
-            html: (<div>Your paper has been uploaded successfully <a style={{color: 'blue'}} href={url}>here</a></div>),
+            html: (<div>Your paper has been uploaded successfully <a style={{ color: 'blue' }} href={url}>here</a></div>),
         });
     }
 
@@ -82,7 +97,7 @@ export default function UploadPaperScreen() {
         return (
             <>
                 <Flex direction="column" alignItems="center">
-                <Heading as='h2' my={4} ml={12}>Review your Paper</Heading>
+                    <Heading as='h2' my={4} ml={12}>Review your Paper</Heading>
                     <Badge fontSize="1rem">{pageNumber}/{numPages}</Badge>
                     <Flex direction="row" alignItems="center">
                         <ChevronLeftIcon cursor="pointer" w={12} h={12} mr={12} onClick={() => {
@@ -108,6 +123,30 @@ export default function UploadPaperScreen() {
         )
     }
 
+    if (abstractScreen) {
+        return (<>
+            <Flex flexDirection="column" justifyContent="center" alignItems="center" height="90vh">
+                <Flex flexDirection="row" alignItems="center">
+                    <img src={logo} alt="logo" width={130} />
+                    <Heading as='h1' size='3xl' marginLeft="2rem">Peer Review</Heading>
+                </Flex>
+                <Flex bgColor="#F8F8FB" py="2rem" px="2rem" mt="2rem" border="2px solid" borderColor="gray.300">
+                    <Flex flexDirection="column" mx={10}>
+                        <FormControl isRequired onChange={retrieveFile}>
+                            <FormLabel>Abstract</FormLabel>
+                            <Textarea w="40rem" h="15rem" d="block" placeholder='Write your abstract here' value={abstract} onChange={e => setAbstract(e.target.value)} />
+                            <Button mt={4} bg='#6459F5' color="#ffffff" variant='solid' onClick={reviewFile}>
+                                Continue
+                            </Button>
+                        </FormControl>
+                    </Flex>
+                </Flex>
+            </Flex>
+        </>
+
+        )
+    }
+
     return (<>
 
         <Flex justifyContent="center" flexDirection="column" alignItems="center" mt="5rem">
@@ -115,16 +154,18 @@ export default function UploadPaperScreen() {
                 <img src={logo} alt="logo" width={130} />
                 <Heading as='h1' size='3xl' marginLeft="2rem">Peer Review</Heading>
             </Flex>
-            <Flex bgColor="#F8F8FB" px={30} py="4rem" mt="4rem" border="2px solid" borderColor="gray.300">
+            <Flex bgColor="#F8F8FB" px={30} py="3rem" mt="3rem" border="2px solid" borderColor="gray.300">
                 <Flex flexDirection="column" mx={10}>
                     <FormControl isRequired onChange={retrieveFile}>
                         <FormLabel>Title</FormLabel>
-                        <Input id='title' type='text' />
-                        <FormLabel mt="0.5rem">Progress</FormLabel>
-                        <Progress colorScheme='green' size='md' value={progress} />
-                        <FormLabel mt="2rem">Attach your paper <LinkIcon /></FormLabel>
+                        <Input id='title' type='text' value={title} onChange={e => setTitle(e.target.value)} />
+                        <FormLabel mt="1rem">Category</FormLabel>
+                        {showOptions()}
+                        <FormLabel mt="1rem">Attach your paper <LinkIcon /></FormLabel>
 
                         <Input type='file' id='formFile' onChange={retrieveFile} />
+                        <FormLabel mt="0.5rem">Progress</FormLabel>
+                        <Progress colorScheme='green' size='md' value={progress} />
                         <Button mt={4} bg='#6459F5' color="#ffffff" variant='solid' onClick={reviewFile}>
                             Continue
                         </Button>
