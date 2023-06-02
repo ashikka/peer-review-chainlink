3981671cfd1a73e1695e1e9773e271de4fd0ce33
ashikka <ashikagupta28@gmail.com>
2023-06-02 19:42:52 +0530

    fix: Make height of cards constant

--- frontend/src/components/PaperCard/PaperCard.tsx
+++ frontend/src/components/PaperCard/PaperCard.tsx
@@ -23,32 +23,34 @@ export default function PaperCard({ title, status, abstract, ipfsHash, heightPer
             p={6}
             overflow={'hidden'}>
             <Flex
-                h={'20vh'}
+                h={200}
                 bg={'gray.100'}
                 mt={-6}
                 mx={-6}
                 mb={6}
                 justifyContent="center">
-                <Document file={ipfsHash}>
-                    <Page height={window.innerHeight * (heightPercentage || 0.7)} pageNumber={1} />
+                <Document file={ipfsHash} >
+                    <Page height={200} pageNumber={1} />
                 </Document>
             </Flex>
-            <Stack>
-                <Heading
-                    color={useColorModeValue('gray.700', 'white')}
-                    fontSize={'l'}
-                    fontFamily={'body'}>
-                    {title}
-                </Heading>
-                <Text color={'gray.500'} noOfLines={[1, 2]}>
-                    {abstract}
-                </Text>
-            </Stack>
-            <Stack mt={2} direction={'row'} spacing={4} align={'center'}>
-                <Stack direction={'column'} spacing={0} fontSize={'sm'}>
-                    <Text mb="0.5rem" fontWeight={600}>{category}</Text>
-                    {(status === "PUBLISHED") ? <Badge textAlign="center" colorScheme="green" w={120}>{status}</Badge> : ((status === "Rejected") ? <Badge textAlign="center" colorScheme="red" w={120}>{status}</Badge> : <Badge textAlign="center" colorScheme='yellow' w={120}>{status}</Badge>)}
+            <Stack height={160}>
+                <Stack>
+                    <Heading
+                        color={useColorModeValue('gray.700', 'white')}
+                        fontSize={'l'}
+                        fontFamily={'body'}>
+                        {title}
+                    </Heading>
+                    <Text color={'gray.500'} noOfLines={[1, 2]}>
+                        {abstract}
+                    </Text>
+                </Stack>
+                <Stack mt={2} direction={'row'} spacing={4} align={'center'}>
+                    <Stack direction={'column'} spacing={0} fontSize={'sm'}>
+                        <Text mb="0.5rem" fontWeight={600}>{category}</Text>
+                        {(status === "PUBLISHED") ? <Badge textAlign="center" colorScheme="green" w={120}>{status}</Badge> : ((status === "Rejected") ? <Badge textAlign="center" colorScheme="red" w={120}>{status}</Badge> : <Badge textAlign="center" colorScheme='yellow' w={120}>{status}</Badge>)}
 
+                    </Stack>
                 </Stack>
             </Stack>
         </Box>
